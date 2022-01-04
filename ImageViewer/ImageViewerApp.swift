2c03b1fcb317085b483e0af8d78fd581c0149bcb
SebastianMorado <sabby.morado@gmail.com>
2022-01-04 15:29:43 +0800

    Initial Commit

---
+++ ImageViewer/ImageViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewerApp.swift
+//  ImageViewer
+//
+//  Created by Sebastian Morado on 1/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ImageViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
